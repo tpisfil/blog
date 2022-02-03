d8db6e873c036c913a4ac4a984e1d76be90b78f0
tpisfil <tpisfil13@gmail.com>
2022-02-03 15:02:24 -0500

    still working on it
    uploading to github, it's not working fully yet, as the title says, I'm still working on it

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="mb-20">
+            <h1 className="sm:text-4xl text-2xl font-bold mt-6 pt-20">This is about us</h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae officia accusantium nulla debitis aut quae exercitationem at soluta maxime. Debitis omnis est quibusdam repudiandae eius, doloremque laborum velit dolores consectetur, atque voluptate unde deserunt voluptatem veniam aliquid pariatur. Minima corrupti aspernatur perferendis corporis debitis libero eligendi a laudantium culpa sit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae officia accusantium nulla debitis aut quae exercitationem at soluta maxime. Debitis omnis est quibusdam repudiandae eius, doloremque laborum velit dolores consectetur, atque voluptate unde deserunt voluptatem veniam aliquid pariatur. Minima corrupti aspernatur perferendis corporis debitis libero eligendi a laudantium culpa sit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae officia accusantium nulla debitis aut quae exercitationem at soluta maxime. Debitis omnis est quibusdam repudiandae eius, doloremque laborum velit dolores consectetur, atque voluptate unde deserunt voluptatem veniam aliquid pariatur. Minima corrupti aspernatur perferendis corporis debitis libero eligendi a laudantium culpa sit.</p>
+        </div>
+    );
+};
+
+
+export default About;
