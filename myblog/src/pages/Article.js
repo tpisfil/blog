0f8800158f182c4f5b3c2cd17117cea5c5011c8b
tpisfil <tpisfil13@gmail.com>
2022-03-02 18:28:26 -0500

    A lot of backend stuff

--- myblog/src/pages/Article.js
+++ myblog/src/pages/Article.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import articleContent from './article-content';
 import Articles from '../components/Articles';
 import NotFound from './NotFound';
 
 const Article = ({match}) => {
-
     const name = match.params.name;
     const article = articleContent.find( article => article.name === name );
     // const article = articleContent.find( articlehello => articlehello.name === name );
+
+    const [articleInfo, setArticleInfo] = useState({
+        comments: []
+    }); 
+    //state gives us a place to temporarily load our information that we call from the server
+    //so in this case its going to be holding the comments 
+    //you can usually have it as an empty object but we added the property comments
+
+    //useEffect runs every time this component is called 
+    //we usually want this so that it already has our data from whatever server or API 
+    //we need already ready for us to use once the page has been loaded up. 
+
+    useEffect(() => {
+        const fetchData = async () => { //useEffect cannot use an async function so we have to create one inside of it
+            const result = await fetch(`http://localhost:8000/api/articles/${name}`);
+            //await will make sure we wait to fetch the info from the server and then move on with the rest of the code
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body); 
+        };
+        fetchData(); //now we're calling the function that we just defined up there ^ 
+    }, [name]); 
+
 
     if (!article) return <NotFound />
 
