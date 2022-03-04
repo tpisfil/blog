858b72bfa7cea8852114caf5f17366ee50505b23
tpisfil <tpisfil13@gmail.com>
2022-03-03 22:39:58 -0500

    node mods on gh shouldnt be an issue anymore

--- myblog-backend/src/server.js
+++ myblog-backend/src/server.js
@@ -7,7 +7,8 @@ app.use(bodyParser.json());
 
 // app.get("/hello", (req,res) => res.send("Hello"));
 // app.post("/hello", (req,res) => res.send(`Hello ${req.body.name}`));
-// app.get("/hello/:name", (req,res) => res.send(`Hello, ${req.params.name}`));
+// app.get("/hello/:name", (req,res) => res.send(`Hello, ${req.params.name}`)); 
+//test comment
 
 const withDB = async (operations, res) => {
     try{
